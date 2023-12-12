93a770cec4f89293049d6aa14b819133afc839ea
maltelueken <malte_lueken@arcor.de>
2023-12-12 10:01:32 +0100

    Add polygons and refactor rectangles to child class of polygons

--- tests/testthat/test_objects.R
+++ tests/testthat/test_objects.R
@@ -25,74 +25,48 @@ testthat::test_that("Object is abstract base class", {
 })
 
 testthat::test_that("Rectangle initialization works", {
-    testthat::expect_no_error(predped::rectangle(lower = c(0, 0), upper = c(1, 1)))
     testthat::expect_no_error(predped::rectangle(center = c(0, 0), size = c(1, 1)))
-    testthat::expect_error(predped::rectangle(center = c(0, 0), lower = c(1, 1)))
-    testthat::expect_error(predped::rectangle(size = c(0, 0), lower = c(1, 1)))
-    testthat::expect_error(predped::rectangle(lower = c(0, 0), upper = c(0, 1)))
+    testthat::expect_error(predped::rectangle(center = c(0, 0), size = 1))
+    testthat::expect_error(predped::rectangle(center = c(0, 0, 0), size = c(1, 1)))
 })
 
 testthat::test_that("Rectangle moving works", {
     r <- predped::move(predped::rectangle(
-        lower = c(0, 0),
-        upper = c(1, 1),
+        center = c(0, 0),
+        size = c(1, 1),
         moveable = TRUE
     ), c(1, 1))
     testthat::expect_equal(r@center, predped::coordinate(c(1, 1)))
     r <- predped::move(predped::rectangle(
-        lower = c(0, 0),
-        upper = c(1, 1),
+        center = c(0, 0),
+        size = c(1, 1),
         moveable = FALSE
     ), c(1, 1))
-    testthat::expect_equal(r@center, predped::coordinate(c(0.5, 0.5)))
+    testthat::expect_equal(r@center, predped::coordinate(c(0, 0)))
 })
 
 testthat::test_that("Rectangle rotation works", {
-    ref <- list(
+    ref <- rbind(
         predped::coordinate(c(1.5, 0.5)),
         predped::coordinate(c(-0.5, 0.5)),
         predped::coordinate(c(-0.5, 1.5)),
         predped::coordinate(c(1.5, 1.5))
     )
     r <- predped::rotate(predped::rectangle(
-        lower = c(0, 0),
-        upper = c(1, 2)
+        center = c(0.5, 1),
+        size = c(1, 2)
     ), radians = pi/2)
-    testthat::expect_equal(unname(predped::corners(r)), ref)
+    testthat::expect_equal(r@points, ref)
     r <- predped::rotate(predped::rectangle(
-        lower = c(0, 0),
-        upper = c(1, 2)
+        center = c(0.5, 1),
+        size = c(1, 2)
     ), degrees = 90)
-    testthat::expect_equal(unname(predped::corners(r)), ref)
+    testthat::expect_equal(r@points, ref)
     r <- predped::rotate(predped::rectangle(
         center = c(0.5, 1),
         size = c(1, 2)
     ), radians = pi/2)
-    testthat::expect_equal(unname(predped::corners(r)), ref)
-})
-
-testthat::test_that("Rectangle corners method works", {
-    r <- predped::rectangle(
-        lower = c(0, 0),
-        upper = c(1, 2)
-    )
-    testthat::expect_equal(predped::corners(r), list(
-        lowerleft = predped::coordinate(c(0, 0)),
-        upperleft = predped::coordinate(c(0, 2)),
-        upperright = predped::coordinate(c(1, 2)),
-        lowerright = predped::coordinate(c(1, 0))
-    ))
-})
-
-testthat::test_that("Rectangle in_object method works", {
-    r <- predped::rectangle(
-        lower = c(0, 0),
-        upper = c(2, 2)
-    )
-    testthat::expect_true(predped::in_object(r, c(1, 1), outside = FALSE))
-    testthat::expect_false(predped::in_object(r, c(1, 1), outside = TRUE))
-    testthat::expect_false(predped::in_object(r, c(3, 3), outside = FALSE))
-    testthat::expect_true(predped::in_object(r, c(3,3), outside = TRUE))
+    testthat::expect_equal(r@points, ref)
 })
 
 testthat::test_that("Circle initialization works", {
