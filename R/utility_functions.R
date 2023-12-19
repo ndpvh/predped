52aa6eadd60d50001710b47d06ead48a88a48671
ndpvh <niels.vanhasbroeck@gmail.com>
2023-12-19 10:47:31 +0100

    Add separation of object and agent: To change later!

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -10,3 +10,22 @@
 find_class <- function(class_name, lst) {
     return(Filter(function(x) class_name %in% class(x), lst))
 }
+
+#' Separate agent and object
+#' 
+#' This function extracts agents and objects from a single `state` list.
+#' 
+#' @param state A list containing the current state of the simulation
+#' 
+#' @return Two lists containing the objects or the agents
+separate_agent_object <- function(state) {
+    # Identify the agents from the `state` list
+    agents <- find_class("agent", state)
+
+    # Identify the objects and delete the agents from this list
+    objects <- find_class("object", state) 
+    objects[[objects %in% agents]] <- NULL 
+
+    return(list("agents" = agents, 
+                "objects" = objects))
+}
