12a5ba05e0285f17943184e3ba7ddd44cf7fad59
ndpvh <niels.vanhasbroeck@gmail.com>
2024-01-04 14:19:57 +0100

    Add the `Iangle` function (untouched)

--- R/geometry.R
+++ R/geometry.R
@@ -150,3 +150,27 @@ scale_velocity <- function(x, time_step = 0.5) {
 angle_to_L2 <- function(x) {
     cbind(x = cos(x * pi / 180), y = sin(x * pi / 180))
 }
+
+# Which angle cone (1..11, NA means outside of view) is p2 in relative to 
+# p1 heading at angel a1 
+#
+# This function kept original for now, as its scope is not yet clear
+Iangle <- function(p1, a1, p2, border = c(-85, -60, -40, -25, -15, -5, 5, 15, 
+                                          25, 40, 60, 85)) {
+  
+    tomp <- function(x) { # -180 ... 180
+        neg <- x < -180
+        x[neg] <- 360 + x[neg]
+        
+        pos <- x > 180
+        x[pos] <- x[pos] - 360
+        
+        x
+    }
+    
+    a <- (angle2(p1, p2) - a1)
+    
+    out <- 12 - .bincode(tomp(a), border)
+    names(out) <- row.names(p2)
+    out
+}
