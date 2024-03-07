af0a5ad419aa513ae25d2e22c8a336b324e7975e
ndpvh <niels.vanhasbroeck@gmail.com>
2024-03-07 14:57:14 +0100

    Update documentation

--- R/update_state.R
+++ R/update_state.R
@@ -1,20 +1,20 @@
-#' Move an object
-#'
-#' Move an object around the space. This function changes the positions and/or
-#' orientation of the provided object. For the class `agent`, this involves a
-#' computation of the utility of a given movement. For other classes, this
-#' involves checking whether an agent is interacting with the object before it
-#' can move.
-#'
-#' @param object The object of which the position should be moved.
-#' @param ... Additional arguments that can differ for agents or environmental
-#' objects.
-#'
-#' @return The provided object with a changed position and/or orientation
-#'
-#' @export
-#' @docType methods
-#' @rdname move-methods
+# ' Move an object
+# '
+# ' Move an object around the space. This function changes the positions and/or
+# ' orientation of the provided object. For the class `agent`, this involves a
+# ' computation of the utility of a given movement. For other classes, this
+# ' involves checking whether an agent is interacting with the object before it
+# ' can move.
+# '
+# ' @param object The object of which the position should be moved.
+# ' @param ... Additional arguments that can differ for agents or environmental
+# ' objects.
+# '
+# ' @return The provided object with a changed position and/or orientation
+# '
+# ' @export
+# ' @docType methods
+# ' @rdname move-methods
 # setGeneric("move", function(object, ...){})
 
 
