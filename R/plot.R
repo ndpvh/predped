5382c18b18c67bfb37103022b7a47815b2c25357
ndpvh <niels.vanhasbroeck@gmail.com>
2024-03-08 13:52:17 +0100

    Differentiate walkable from unwalkable space

--- R/plot.R
+++ R/plot.R
@@ -6,11 +6,11 @@
 #' @return Either a geom or a ggplot
 #' @export
 #' @name plot-method
-setGeneric("plot", function(object) {standardGeneric("plot")})
+setGeneric("plot", function(object, ...) standardGeneric("plot"))
 
 #'@rdname plot-method
 #'
-setMethod("plot", "circle", function(object) {
+setMethod("plot", "circle", function(object, ...) {
   # Create circle point from center x,y and radius r
   t <- seq(0, 2 * pi, length.out = 100)
   cp <- as.matrix(data.frame(
@@ -21,42 +21,43 @@ setMethod("plot", "circle", function(object) {
   # Plot circle using geom_polygon
   ggplot2::geom_polygon(
     ggplot2::aes(x = cp[, 1], y = cp[, 2]),
-     fill = "white"
+    ...
   )
 })
 
 
 #'@rdname plot-method
 #'
-setMethod("plot", "rectangle", function(object) {
+setMethod("plot", "rectangle", function(object, ...) {
   ggplot2::geom_tile(
     ggplot2::aes(x = object@center[[1]], y = object@center[[2]], width = object@size[[1]], height = object@size[[2]]),
-    fill = "white"
+    ...
   )
 })
 
 #'@rdname plot-method
 #'
-setMethod("plot", "polygon", function(object) {
+setMethod("plot", "polygon", function(object, ...) {
   ggplot2::geom_polygon(
     ggplot2::aes(x = object@points[, 1], y = object@points[, 2]), 
-    fill = "white"
+    ...
   )
 })
 
 #'@rdname plot-method
 #'
-setMethod("plot", "background", function(object) {
+setMethod("plot", "background", function(object, ...) {
   plt <- ggplot2::ggplot() + 
+         predped::plot(shape(object), fill = "white") +
          ggplot2::coord_equal() +
          ggplot2::labs(x = "x", y = "y") +
          ggplot2::theme(
-          panel.background = element_rect(fill = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank()
+          panel.background = ggplot2::element_rect(fill = "black"),
+          panel.grid.major = ggplot2::element_blank(),
+          panel.grid.minor = ggplot2::element_blank()
          )
   for (i in seq_along(objects(object))) {
-    plt <- plt + plot(objects(object)[[i]])
+    plt <- plt + plot(objects(object)[[i]], ...)
   }
   return(plt)
 })
