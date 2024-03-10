0676c1aa5a52f894ee3741ac154db01f3bb79eae
eceyatikci <152717887+eceyatikci@users.noreply.github.com>
2024-03-10 10:23:55 +0100

    Add class for Shelf Distribution

--- RandomShelvesGenerator.R
+++ RandomShelvesGenerator.R
@@ -1,65 +1,50 @@
 ## Generate a Random Distribution of Shelves 
+# Class for Shelf Distribution
 
-# set seed for reproducibility
-set.seed(4355)  
+# set a class for shelf distribution
+ShelfDistribution <- setClass("ShelfDistribution",
+                               slots = list(num_cols = "numeric",
+                                            num_shelves = "numeric",
+                                            shelves_dist = "numeric"))
 
-# generate random nr of cols and shelves
-num_cols <- sample(1:10, 1) # random nr of cols between 1-10
-num_shelves <- max(sample(1:20, 1), num_cols) # random nr of shelves between 1-20
+# define the generic
+setGeneric("generate_dist",
+           function(object) {
+               standardGeneric("generate_dist")
+           })
 
-# for checks
-num_cols 
-num_shelves 
+# define a method to generate the distribution
+setMethod("generate_dist", "ShelfDistribution", function(object) {
+    num_cols <- sample(1:10, 1)  # random nr of cols between 1-10
+    num_shelves <- max(sample(1:20, 1), num_cols)  # random nr of shelves between 1-20
 
-# initialize the distribution of shelves
-shelves_dist <- rep(0, num_cols)
+    # initialize the distribution
+    shelves_dist <- rep(0, num_cols)
 
-# make sure each column has at least one shelf
-for (i in 1:min(num_shelves, num_cols)) {
-  shelves_dist[i] <- 1 
-}
+    # make sure each column has at least one shelf
+    for (i in 1:min(num_shelves, num_cols)) {
+        shelves_dist[i] <- 1 
+    }
 
-# update the number of remaining shelves
-num_shelves <- num_shelves - sum(shelves_dist)
+    # update the num_shelves
+    num_shelves <- num_shelves - sum(shelves_dist)
 
-# randomly distribute remaining shelves to columns
-for (i in 1:num_shelves) {
-  col_index <- sample(1:num_cols, 1)  # randomly select a column to add a shelf
-  shelves_dist[col_index] <- shelves_dist[col_index] + 1
-}
+    # distribute remaining shelves
+    while (num_shelves > 0) {
+        col_index <- sample(1:num_cols, 1) 
+        shelves_dist[col_index] <- shelves_dist[col_index] + 1
+        num_shelves <- num_shelves - 1
+    }
 
-# print the distribution
-data <- data.frame(Columns = 1:num_cols, Num_Shelves = shelves_dist)
-print(data)
+    # update the object slots
+    object@num_cols <- num_cols
+    object@num_shelves <- sum(shelves_dist)
+    object@shelves_dist <- shelves_dist
 
-# plot the distribution
-library(ggplot2)
+    return(object)
+})
 
-# empty df for storing helf data
-shelf_data <- data.frame(Column = integer(), Shelf = integer())
-
-# iterate through each row of the distribution
-for (i in 1:nrow(data)) {
-  # generate col numbers based on num_shelves in each column
-  columns <- rep(data$Columns[i], data$Num_Shelves[i])
-  
-  # generate shelf numbers based on the num_shelves in each column
-  shelves <- 1:data$Num_Shelves[i]
-  
-  # temporary df for the current distribution
-  temp_df <- data.frame(Column = columns, Shelf = shelves)
-  
-  # combine temp_df with the shelf_data
-  shelf_data <- rbind(shelf_data, temp_df)
-}
-
-# Plot the shelves
-ggplot(shelf_data, aes(x = Column, y = Shelf)) +
-  # add padding of 0.5 to the sides for the block
-  geom_rect(aes(xmin = Column - 0.5, xmax = Column + 0.5,
-                ymin = Shelf - 0.5, ymax = Shelf + 0.5),
-            fill = "blue", color = "black") +
-  labs(title = "Distribution of Shelves",
-       x = "Column",
-       y = "Shelf") +
-  theme_minimal()
+# an example for ShelfDistribution Class
+shelf_dist <- new("ShelfDistribution", num_cols = num_cols, num_shelves = num_shelves)
+shelf_dist <- generate_dist(shelf_dist)
+print(shelf_dist)
