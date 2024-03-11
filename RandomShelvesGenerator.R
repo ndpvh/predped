957b8249d77483a76dc30035a1bd0366901e267f
eceyatikci <152717887+eceyatikci@users.noreply.github.com>
2024-03-11 13:39:49 +0100

    Get coordinates correctly

--- RandomShelvesGenerator.R
+++ RandomShelvesGenerator.R
@@ -6,6 +6,14 @@ ShelfDistribution <- setClass("ShelfDistribution",
                                             num_shelves = "numeric",
                                             shelves_dist = "numeric"))
 
+# set an initialization for the Class ShelfDistribution
+setMethod("initialize", "ShelfDistribution", function(.Object, num_cols, num_shelves, shelves_dist, ...) {
+    .Object@num_cols <- num_cols
+    .Object@num_shelves <- num_shelves
+    .Object@shelves_dist <- shelves_dist
+    return(.Object)
+})
+
 # define the generic
 setGeneric("generate_dist",
            function(object) {
@@ -65,28 +73,30 @@ setMethod("initialize", "ShelfRectangle", function(.Object, center, size, orient
 
 # a method to plot shelf distribution using ShelfRectangle
 # it should not be a df, should be changed
-setMethod("plot", "ShelfDistribution", function(object, shelf_length = 1, shelf_width = 0.5, aisle_width = 1.5, ...) {
-    shelf_rectangles <- vector("list", length = sum(object@shelves_dist))
+# define the generic
+setGeneric("getShelfCoordinates",
+           function(object, ...) {
+               standardGeneric("getShelfCoordinates")
+           })
+
+setMethod("getShelfCoordinates", "ShelfDistribution", function(object, shelf_length = 1, shelf_width = 0.5, aisle_width = 1.5, ...) {
+    shelf_coordinates <- data.frame(xmin = numeric(0), xmax = numeric(0), ymin = numeric(0), ymax = numeric(0))
     index <- 1
     for (col in 1:object@num_cols) {
         for (shelf in 1:object@shelves_dist[col]) {
             shelf_center <- c((col - 1) * (shelf_width + aisle_width) + shelf_width / 2, shelf)
-            shelf_rectangle <- new("ShelfRectangle", center = shelf_center, size = c(shelf_width, shelf_length), distribution = object)
-            shelf_rectangles[[index]] <- shelf_rectangle
+            shelf_coordinates[index, ] <- c(shelf_center[1] - shelf_width/2, shelf_center[1] + shelf_width/2, shelf - shelf_length/2, shelf + shelf_length/2)
             index <- index + 1
         }
     }
-    ggplot2::ggplot() +
-        ggplot2::geom_blank(data = data.frame(x = c(0, object@num_cols * (shelf_width + aisle_width)), y = c(0, max(object@shelves_dist) + 1)), aes(x, y)) +
-        ggplot2::coord_fixed() +
-        ggplot2::theme_void() +
-        ggplot2::theme(panel.grid = ggplot2::element_blank()) +
-        lapply(shelf_rectangles, plot, ...) +
-        ggplot2::scale_x_continuous(expand = c(0, 0))
+    return(shelf_coordinates)
 })
 
-# example use & plot the shelf distribution
-shelf_dist <- new("ShelfDistribution")
-shelf_dist <- generate_dist(shelf_dist)
-plot(shelf_dist, shelf_length = 1, shelf_width = 0.5, aisle_width = 1.5)
 
+# example use & plot the shelf distribution
+# shelf_dist <- new("ShelfDistribution")
+shelf_dist <- generate_dist(shelf_dist)
+getShelfCoordinates(shelf_dist, shelf_length = 1, shelf_width = 0.5, aisle_width = 1.5)
+print(shelf_dist)
+
+## add circles
