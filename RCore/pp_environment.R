74873f93b0f28b5089338a88df2808ae010e049d
ndpvh <niels.vanhasbroeck@gmail.com>
2024-03-27 10:49:00 +0100

    Delete RCore

--- RCore/pp_environment.R
+++
@@ -1,5 +0,0 @@
-#######################################################
-# PURPOSE: Define environment-related functions       #
-#          that will be used to create and maintain   #
-#          a given environment                        #
-#######################################################
