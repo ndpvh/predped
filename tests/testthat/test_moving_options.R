4159787e83c0ebc2077b385a22d6ee1b7ee8dfde
ndpvh <niels.vanhasbroeck@gmail.com>
2024-03-27 15:06:37 +0100

    Fix tests

--- tests/testthat/test_moving_options.R
+++ tests/testthat/test_moving_options.R
@@ -7,36 +7,36 @@ testthat::test_that("Number of pedestrians blocking view from goal works", {
     me <- predped::agent(center = c(-4, 0), 
                          radius = 0.2, 
                          orientation = 0,
-                         goals = list(predped::goal(position = c(4, 0))))
+                         current_goal = predped::goal(position = c(4, 0)))
 
     # Create some blocking and non-blocking pedestrians
     block_1 <- predped::agent(center = c(-2, 0), radius = 0.2)
     block_2 <- predped::agent(center = c(2, 0), radius = 0.2)
-    noblock_1 <- predped::agent(center = c(-2, 0.5) , radius = 0.2)
-    noblock_2 <- predped::agent(center = c(-2, -0.5) , radius = 0.2)
+    noblock_1 <- predped::agent(center = c(-2, 0.75) , radius = 0.2)
+    noblock_2 <- predped::agent(center = c(-2, -0.75) , radius = 0.2)
 
     # Try several combinations and see whether the correct number of blocking
     # agents are derived
     state_0a <- list(setting = mock_setting, 
-                     agents = list(noblock_1, noblock_2))
+                     agents = list(me, noblock_1, noblock_2))
     state_0b <- list(setting = mock_setting, 
-                     agents = list(noblock_1))
+                     agents = list(me, noblock_1))
     state_0c <- list(setting = mock_setting, 
-                     agents = list(noblock_2))
+                     agents = list(me, noblock_2))
 
     state_1a <- list(setting = mock_setting, 
-                     agents = list(block_1))
+                     agents = list(me, block_1))
     state_1b <- list(setting = mock_setting, 
-                     agents = list(noblock_1, block_1))
+                     agents = list(me, noblock_1, block_1))
     state_1c <- list(setting = mock_setting, 
-                     agents = list(noblock_2, block_2))
+                     agents = list(me, noblock_2, block_2))
 
     state_2a <- list(setting = mock_setting, 
-                     agents = list(block_1, block_2))
+                     agents = list(me, block_1, block_2))
     state_2b <- list(setting = mock_setting, 
-                     agents = list(block_1, noblock_1, block_2))
+                     agents = list(me, block_1, noblock_1, block_2))
     state_2c <- list(setting = mock_setting, 
-                     agents = list(block_2, noblock_2, block_2))
+                     agents = list(me, block_2, noblock_2, block_2))
 
     # Put them all in a list
     states <- list(state_0a, state_0b, state_0c,
