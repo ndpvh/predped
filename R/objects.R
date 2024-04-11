52094571693723b042f5ec253f127bc4f252f8c5
ndpvh <niels.vanhasbroeck@gmail.com>
2024-04-11 16:32:30 +0200

    Delete a sqrt where not necessary

--- R/objects.R
+++ R/objects.R
@@ -753,9 +753,9 @@ setMethod("in_object", signature(object = "circle"), function(object, x, outside
     # If this distance is smaller than the radius, the point is within the 
     # circle.
     y <- center(object)
-    dist <- sqrt((x[1] - y[1])^2 + (x[2] - y[2])^2)
+    dist <- (x[1] - y[1])^2 + (x[2] - y[2])^2
 
-    check <- dist < radius(object)
+    check <- dist < radius(object)^2
     return(ifelse(outside, !check, check))
 })
 
