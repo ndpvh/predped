ffa95fe539748864f6f7f8f4b91a5bff00cf1cc9
ndpvh <niels.vanhasbroeck@gmail.com>
2024-08-07 21:41:12 +0200

    Create additional test for `line_line_interaction`

--- tests/testthat/test_general.R
+++ tests/testthat/test_general.R
@@ -118,3 +118,59 @@ testthat::test_that("Vectorized line-line intersection works", {
     testthat::expect_false(predped::line_line_intersection(tst1, tst3))
     testthat::expect_false(predped::line_line_intersection(tst1, tst3)) # To change once figured out how to deal with this
 })
+
+testthat::test_that("Vectorized line-line intersections are interchangeable", {
+    # For this test, we need one shorter array of segments and one longer: 
+    #
+    # The underlying function will always vectorize over the longer one, but 
+    # should give the correct results related to the segments in `tst1` first.
+    # 
+    # The idea behind this function is to check whether the order of the TRUE's 
+    # and FALSE's that is present in the comparison of tst1 to tst2 is the same 
+    # order when tst1 is elongated (in tst3). If this is the case, then we know 
+    # that under the hood, the order of things is preserved. To make sure the 
+    # test is valid, we do this once in one direction (from tst1 to tst2) and 
+    # once in the other direction (from tst2 to tst1)
+    tst1 <- rbind(c(1, 1, 4, 4), 
+                  c(3, -2, 6, -2), 
+                  c(-3, -1, -3, -5))
+    tst2 <- rbind(c(1, 2, 5, 2), 
+                  c(5, 1, 5, -3), 
+                  c(0, -3, -4, -3),                   
+                  c(4, -3, 3, 4),
+                  c(-4, -5, 6, -1),
+                  c(-4, -6, 4, 5),
+
+                  c(-3, 2, 1, 5),
+                  c(-4, -1, -4, -4),
+                  c(7, -1, 10, -1),
+                  c(-3, -5, -3, -8),
+                  c(6, -2, 9, -2),
+                  c(4, 4, 7, 7))
+    tst3 <- rbind(tst1, tst1, tst1, tst1, tst1)
+
+    # Build the reference
+    ref <- cbind(c(TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, rep(FALSE, 6)),
+                 c(FALSE, TRUE, FALSE, TRUE, TRUE, FALSE, rep(FALSE, 6)),
+                 c(FALSE, FALSE, TRUE, FALSE, TRUE, TRUE, rep(FALSE, 6)))
+    
+    # Do the first test
+    ref_1 <- as.logical(ref)
+    ref_2 <- rep(ref_1, times = 5)
+
+    tst_1 <- predped::line_line_intersection(tst1, tst2, return_all = TRUE)
+    tst_2 <- predped::line_line_intersection(tst3, tst2, return_all = TRUE)
+
+    testthat::expect_equal(tst_1, ref_1)
+    testthat::expect_equal(tst_2, ref_2)
+
+    # Do the second test
+    ref_1 <- as.logical(t(ref))
+    ref_2 <- as.logical(rbind(t(ref), t(ref), t(ref), t(ref), t(ref))) # Given that tst3 is the repeat, we need to do it like this
+
+    tst_1 <- predped::line_line_intersection(tst2, tst1, return_all = TRUE)
+    tst_2 <- predped::line_line_intersection(tst2, tst3, return_all = TRUE)
+
+    testthat::expect_equal(tst_1, ref_1)
+    testthat::expect_equal(tst_2, ref_2)
+})
