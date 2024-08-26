5620a52c32a253fc087d187acf86b8e332ec7be4
Alexanderson31 <alexander.anderson@student.uva.nl>
2024-08-26 11:58:06 +0200

    Change to gc_utility. Now make optimal distance accomodate group sizes.

--- R/group_centroid_utility.R
+++ R/group_centroid_utility.R
@@ -6,21 +6,17 @@
 #'          coordinates of pedestrians belonging to the group of pedestrian `n`.
 #' @details Returns `NULL` if the pedestrian `n` does not belong to a pedestrian social group.
 #'
-#' @param agent_idx Integer scalar indexing the pedestrian in the state.
 #' @param p_pred Numeric matrix with shape Nx2 (x & y) containing predicted positions
 #' of all pedestrians.
-#' @param agent_group Numeric vector with group membership indices of all pedestrians.
 #' @param centers Numeric matrix with shape (33x2) containing (x & y) for each cell centre.
+#' @param nped Numeric integer indicating number of people in pedestrian `n`'s social group. 
 #'
 #' @return Numeric vector `cell_dist` with distances from each cell center to the group centroid. 
 #'
 #' @export 
-get_mean_group_centroid <- function(agent_idx, p_pred, agent_group, centers) {
+get_mean_group_centroid <- function(p_pred, centers, nped) {
    
     # First need to identify whether a pedestrian belongs to a social group
-    inGroup <- agent_group[-agent_idx] == agent_group[agent_idx]
-    p_pred <- p_pred[inGroup, , drop = FALSE]
-    nped <- dim(p_pred)[1]
     
     if (nped == 0) {
         return(NULL)    
@@ -76,22 +72,25 @@ get_median_group_centroid <- function(agent_idx, p_pred, agent_group, centers) {
 #'
 #' @param a_gc Numeric scalar power parameter
 #' @param b_gc Numeric scalar steepness parameter
-#' @param optm_d Numeric scalar indicating optimal distance to group centroid.
+#' @param radius Numeric double indicating the radius of pedestrian `n`.
 #' @param cell_dist Numeric vector with the distance from the cell to the predicted group centroid.
-#' @param stop_utility ...
+#' @param stop_utility Numeric double with the disutility of 34th stopping cell.
+#' @param nped Numeric integer indicating number of people in pedestrian `n`'s social group. 
 #' 
 #' @return Numeric vector `centroid_util` with group centroid utility for each cell.
 #' @export
 #'
-gc_utility <- function(a_gc, b_gc, optm_d, cell_dist, stop_utility) {
+gc_utility <- function(a_gc, b_gc, radius, cell_dist, stop_utility, nped) {
     
     if (is.null(cell_dist)) {
         return(numeric(33))
     }
+
+    optimal_distance <- 1.5 * nped * radius
     
     # Calculate centroid 
     centroid_util <- -sapply(cell_dist, 
-                                \(x) ifelse(x < optm_d, 0, b_gc * (x - optm_d)^a_gc))
+                                \(x) ifelse(x < optimal_distance, 0, b_gc * (x - optimal_distance)^a_gc))
 
     if (all(centroid_util < stop_utility)) {
         return(numeric(33))
