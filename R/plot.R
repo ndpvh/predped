b4a7c91949657197f87520e3615b61b5fb7a16be
ndpvh <niels.vanhasbroeck@gmail.com>
2024-09-18 17:11:25 +0200

    Create plot for `state`

--- R/plot.R
+++ R/plot.R
@@ -47,152 +47,41 @@ setMethod("plot", "polygon", function(object, ...) {
 #'@rdname plot-method
 #'
 setMethod("plot", "agent", function(object, plot_goal = TRUE,...) {
-  angle <- object@orientation * 2 * pi / 360
-  plt <- list(plot(circle(center = object@center, 
-                          radius = object@radius), 
-                   fill = NA,
-                   color = object@color,
-                   ...),
-              ggplot2::annotate("segment", 
-                                x = object@center[[1]], 
-                                y = object@center[[2]], 
-                                xend = object@center[[1]] + object@radius * cos(angle), 
-                                yend = object@center[[2]] + object@radius * sin(angle), 
-                                color = object@color,
-                                ...))
+    # Determine the orientation of the agent to be plotted
+    angle <- object@orientation * 2 * pi / 360
 
-    if(plot_goal) {
-        plt <- append(plt, 
-                      ggplot2::geom_point(ggplot2::aes(x = current_goal(object)@position[1],
-                                                       y = current_goal(object)@position[2]),
-                                          color = object@color,
-                                          ...))
-    }
+    # Plot a circle with the 
+    plt <- list(plot(circle(center = object@center, 
+                            radius = object@radius), 
+                    fill = NA,
+                    color = object@color,
+                    ...),
+                ggplot2::annotate("segment", 
+                                    x = object@center[[1]], 
+                                    y = object@center[[2]], 
+                                    xend = object@center[[1]] + object@radius * cos(angle), 
+                                    yend = object@center[[2]] + object@radius * sin(angle), 
+                                    color = object@color,
+                                    ...))
+
+        if(plot_goal) {
+            plt <- append(plt, 
+                        ggplot2::geom_point(ggplot2::aes(x = current_goal(object)@position[1],
+                                                        y = current_goal(object)@position[2]),
+                                            color = object@color,
+                                            ...))
+        }
 
     return(plt)
 })
-
-#'@rdname plot-method
-#'
-setMethod("plot", "list", function(object, 
-                                   trace = FALSE, 
-                                   print_progress = TRUE, 
-                                   iterations = NULL,
-                                   ...) {
-    # If the list in question is the trace, then we have to output the plots for
-    # each state in the simulation. This is a little more complicated than for 
-    # a simple list
-    plt <- list()
-    if(trace) {
-        if(print_progress) {
-            cat("\n")
-        }
-        
-        # Setting doesn't change, so can be saved immediately
-        base_plot <- predped::plot(object[[1]]$setting, 
-                                   fill = "grey", 
-                                   color = "black")
-
-        # Loop over each state
-        for(i in seq_along(object)) {
-            iter <- ifelse(is.null(iterations[i]), i, iterations[i])
-
-            if(print_progress) {
-                cat(paste0("\rMaking plot for iteration ", iter))
-            }
-
-            # If there are currently no agents, then we just return the base_plot
-            if(length(object[[i]]$agents) == 0) {
-                plt[[i]] <- base_plot
-
-            # Otherwise, we will have to add the agents in the base_plot
-            } else {
-                # Transform the complete state to a collection of segments
-                segments <- matrix(0, nrow = 0, ncol = 5)
-                goals <- matrix(0, nrow = 0, ncol = 3)
-                color_code <- c()
-                for(j in object[[i]]$agents) {
-                    # Get coordinates of the agent themselves and turn into 
-                    # segments
-                    if(inherits(j, "circle")) {
-                        agent_coords <- points(j, length.out = 25)
-                    } else {
-                        agent_coords <- j@points
-                    }
-                    agent_segments <- cbind(agent_coords, 
-                                            agent_coords[c(2:nrow(agent_coords), 1),])
-
-                    # Keep track of the location of the agent's goals. Furthermore
-                    # add a segment that denotes the orientation of the agent
-                    agent_goals <- current_goal(j)@position
-
-                    angle <- j@orientation * 2 * pi / 360
-                    agent_segments <- rbind(agent_segments, 
-                                            c(center(j), 
-                                              center(j) + radius(j) * c(cos(angle), sin(angle))))
-
-                    # Add the color to the color code if it is not already in 
-                    # there
-                    if(!(j@color %in% color_code)) {
-                        color_code[j@color] <- j@color
-                    }
-
-                    # Add information on the color to the dataframes
-                    agent_segments <- cbind(agent_segments, j@color)
-                    agent_goals <- c(agent_goals, j@color)
-
-                    segments <- rbind(segments, agent_segments)
-                    goals <- rbind(goals, agent_goals)
-                }
-
-                # Once done, plot all this information as a collection of segments 
-                # and points
-                segments <- as.data.frame(segments) |>
-                    setNames(c("x", "y", "xend", "yend", "color"))
-                goals <- as.data.frame(goals) |>
-                    setNames(c("x", "y", "color"))
-
-                plt[[i]] <- suppressWarnings(base_plot +
-                    ggplot2::geom_segment(data = segments, 
-                                          ggplot2::aes(x = as.numeric(x), 
-                                                      y = as.numeric(y), 
-                                                      xend = as.numeric(xend),
-                                                      yend = as.numeric(yend),
-                                                      color = color),
-                                          ...) +
-                    ggplot2::geom_point(data = goals, 
-                                        ggplot2::aes(x = as.numeric(x), 
-                                                    y = as.numeric(y), 
-                                                    color = color),
-                                        ...) +
-                    ggplot2::scale_color_manual(values = color_code)) 
-            }
-
-            plt[[i]] <- plt[[i]] +
-                ggplot2::labs(title = paste("iteration", iter)) +
-                ggplot2::theme(legend.position = "none")
-        }
-
-    # If it is not the trace, then we need to output the list of geom's that are
-    # created for each of the elements in the list
-    } else {
-        for(i in seq_along(object)) {
-            plt <- append(plt, plot(object[[i]], ...))
-        }        
-    }
-
-    if(print_progress) {
-        cat("\n")
-    }
-
-    return(plt)
-})
-
 
 #'@rdname plot-method
 #'
 setMethod("plot", "background", function(object, 
                                          entry_exit_width = 0.4,
+                                         shape.fill = "white",
+                                         object.fill = "grey",
+                                         object.color = "black",
                                          ...) {
     # Not my preferred way of doing things: 
     #
@@ -213,19 +102,25 @@ setMethod("plot", "background", function(object,
     y_padding <- 0.05 * (ylims[2] - ylims[1])
 
     plt <- ggplot2::ggplot() + 
-        predped::plot(shape(object), fill = "white") +
+        # Plot the shape and objects of the environment
+        predped::plot(shape(object), 
+                      fill = shape.fill) +
+        predped::plot(objects(object), 
+                      fill = object.fill, 
+                      color = object.color,
+                      ...) +
+        # Other things to make the plot beautiful
         ggplot2::coord_fixed() +
         ggplot2::scale_x_continuous(limits = c(xlims[1] - x_padding, xlims[2] + x_padding)) +
         ggplot2::scale_y_continuous(limits = c(ylims[1] - y_padding, ylims[2] + y_padding)) +
         ggplot2::labs(x = "x", y = "y") +
-        ggplot2::theme(
-            panel.background = ggplot2::element_rect(fill = "black"),
-            panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank()
-        )
-    for (i in seq_along(objects(object))) {
-        plt <- plt + plot(objects(object)[[i]], ...)
-    }
+        ggplot2::theme(panel.background = ggplot2::element_rect(fill = "black"),
+                       panel.grid.major = ggplot2::element_blank(),
+                       panel.grid.minor = ggplot2::element_blank()) 
+
+    # for (i in seq_along(objects(object))) {
+    #     plt <- plt + plot(objects(object)[[i]], ...)
+    # }
 
     # Convert entrance & exit to polygon coordinates
     entrance <- lapply(seq_len(nrow(object@entrance)), 
@@ -247,5 +142,73 @@ setMethod("plot", "background", function(object,
         plt <- plt + plot(polygon(points = exit[[i]][idx,]), fill = NA, colour = "black")
     }
     
+    return(plt)
+})
+
+#' @rdname plot-method
+#' 
+setMethod("plot", "state", function(object, 
+                                    plot_goal = TRUE,
+                                    plot.title.size = 10,
+                                    plot.title.hjust = 0.5,
+                                    axis.title.size = 10,
+                                    ...) {
+
+    # Create the plot for the setting, which will serve as the basis of 
+    base_plot <- predped::plot(setting(object), ...)
+
+    # If there are currently no agents, then we just return the base_plot
+    if(length(object@agents) == 0) {
+        return(base_plot)
+    }
+    
+    # Otherwise, we will have to add the agents in the base_plot
+    return(base_plot + 
+        predped::plot(agents(object), 
+                      plot_goal = plot_goal,
+                      ...) +
+        ggplot2::labs(title = paste("iteration", object@iteration)) +
+        ggplot2::theme(legend.position = "none",
+                       plot.title = ggplot2::element_text(size = plot.title.size,
+                                                          hjust = plot.title.hjust),
+                       axis.title = ggplot2::element_text(size = axis.title.size)))                
+})
+
+#'@rdname plot-method
+#'
+setMethod("plot", "list", function(object, ...) {
+    
+    # First a check of whether anything is contained within this list. Otherwise
+    # return an error
+    if(length(object) == 0) {
+        stop("No elements are contained within this list")
+    }
+
+    # If the list in question is the trace, then we have to output the plots for
+    # each state in the simulation. Otherwise, we have to output all the geom's 
+    # that are in the trace. In both cases, we can simply dispatch based on the 
+    # elements of the list, and this can thus be as simple as having an lapply
+    #
+    # However, if it is a trace, we would like some information on the iteration
+    # of the state, which we need to do some bookkeeping for
+    trace <- inherits(object[[1]], "state")
+
+    if(trace) {
+        cat("\n")
+    }
+
+    plt <- lapply(seq_along(object),
+                  function(i) {
+                      if(trace) {
+                          cat(paste0("\rMaking plot for state ", object[[i]]@iteration))
+                      }
+
+                      return(predped::plot(object[[i]], ...))
+                  })
+
+    if(trace) {
+        cat("\n")
+    }
+
     return(plt)
 })
