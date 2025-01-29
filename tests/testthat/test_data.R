51e394171240630c9f94641593a5257980a0cc57
ndpvh <niels.vanhasbroeck@gmail.com>
2025-01-29 16:33:54 +0100

    Create test for transformations in data (Issue #38)

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -7,3 +7,21 @@ testthat::test_that("Transforming to time series works", {
 
     testthat::expect_equal(ref, tst)
 })
+
+testthat::test_that("Transforming to trace works", {
+    ref <- readRDS(file.path(".", "data", "example_trace.Rds"))
+
+    # Transform to a dataframe and back
+    data <- predped::time_series(ref)
+    tst <- predped::to_trace(
+        data,
+        ref[[1]]@setting
+    )
+
+    # Do some checks on the trace itself
+    testthat::expect_equal(length(tst), 9)
+    testthat::expect_equal(
+        sapply(tst, \(x) length(x@agents)), 
+        c(0, 1, 1, 1, 1, 2, 2, 2, 2)
+    )
+})
