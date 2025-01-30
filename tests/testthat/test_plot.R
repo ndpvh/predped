bf21d08973f1584683029e9c46387634bfb5d4a7
ndpvh <niels.vanhasbroeck@gmail.com>
2025-01-30 17:23:42 +0100

    Add test of plotting forbidden edges (Issue #42)

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -24,3 +24,60 @@ testthat::test_that("Plotting a polygon works", {
                                                                         ncol = 2, 
                                                                         byrow = T)))) 
 })
+
+testthat::test_that("Plotting forbidden edges works", {
+    my_background <- predped::background(
+        shape = predped::rectangle(
+            center = c(0, 0),
+            size = c(5, 5)
+        )
+    )
+
+    rect <- predped::rectangle(center = c(0, 0), size = c(2, 2))
+    poly <- predped::polygon(points = rbind(c(-1, -1), c(-1, 1), c(1, 1), c(1, -1)))
+    circ <- predped::circle(center = c(0, 0), radius = 1)
+
+    # Rectangles
+    cases <- list(1, 
+                  1:2,
+                  1:4,
+                  c(1, 3),
+                  c(1, 3:4),
+                  c(1:2, 4),
+                  c(4, 1))
+
+    lapply(cases,
+           function(x) {
+              rect@forbidden <- x
+              my_background@objects <- list(rect)
+              testthat::expect_no_error(predped::plot(my_background, plot_forbidden = TRUE)) 
+           })
+
+    # Polygons
+    cases <- list(1, 
+                  1:2,
+                  1:4,
+                  c(1, 3),
+                  c(1, 3:4),
+                  c(1:2, 4),
+                  c(4, 1))
+
+    lapply(cases,
+           function(x) {
+              poly@forbidden <- x
+              my_background@objects <- list(poly)
+              testthat::expect_no_error(predped::plot(my_background, plot_forbidden = TRUE)) 
+           })
+
+    # Circles
+    cases <- list(matrix(c(0, 1, 0.5, 2), nrow = 2),
+                  matrix(c(0.5, 0, 2, 1), nrow = 2),
+                  matrix(c(0, 1), nrow = 1))
+    lapply(cases,
+           function(x) {
+              circ@forbidden <- x
+              my_background@objects <- list(circ)
+              testthat::expect_no_error(predped::plot(my_background, plot_forbidden = TRUE)) 
+           })
+
+})
