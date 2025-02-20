8dfe8c8ad262909b849040b0bf594fcfdac07fb1
ndpvh <niels.vanhasbroeck@gmail.com>
2025-02-20 18:35:05 +0100

    Export additional utility functions

--- src/utility_model.h
+++ src/utility_model.h
@@ -3,9 +3,24 @@
 
 #include <Rcpp.h>
 
+Rcpp::DataFrame compute_utility_variables_rcpp(
+    Rcpp::S4 agent,
+    Rcpp::S4 state,
+    Rcpp::S4 background,
+    Rcpp::List agent_specifications,
+    Rcpp::NumericMatrix centers,                    
+    Rcpp::LogicalMatrix check
+);
+
 Rcpp::NumericVector utility_rcpp(
     Rcpp::DataFrame data, 
     Rcpp::DataFrame parameters
+);
+
+Rcpp::Nullable<Rcpp::NumericVector> distance_group_centroid_rcpp(
+    Rcpp::NumericMatrix predictions, 
+    Rcpp::NumericMatrix centers, 
+    int number_agents
 );
 
 Rcpp::NumericVector gc_utility_rcpp(
@@ -15,6 +30,16 @@ Rcpp::NumericVector gc_utility_rcpp(
     Rcpp::NumericVector cell_distances, 
     double stop_utility, 
     int nped
+);
+
+Rcpp::Nullable<Rcpp::NumericVector> get_angles_rcpp(
+    int agent_idx, 
+    Rcpp::NumericVector agent_groups, 
+    Rcpp::NumericVector position, 
+    double orientation,  
+    Rcpp::NumericMatrix predictions, 
+    Rcpp::NumericMatrix centers,
+    bool any_member = true
 );
 
 Rcpp::NumericVector vf_utility_rcpp(
