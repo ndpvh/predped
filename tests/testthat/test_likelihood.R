f3c7eda737dec806397332dec1a85ffa251d9016
ndpvh <niels.vanhasbroeck@gmail.com>
2025-03-14 11:27:09 +0100

    Adjust benchmarks and tests (Issue #61)

--- tests/testthat/test_likelihood.R
+++ tests/testthat/test_likelihood.R
@@ -23,12 +23,12 @@ testthat::test_that("Computing the MLL works", {
     params_max$a_current_direction <- 2
 
     # Compute the min-log-likelihood for each of the parameters
-    best <- predped::mll(data, params, transform = FALSE)
+    best <- predped::mll(data, params, transform = FALSE, summed = TRUE)
 
-    min <- predped::mll(data, params_min, transform = FALSE)
-    q25 <- predped::mll(data, params_25, transform = FALSE)
-    q75 <- predped::mll(data, params_75, transform = FALSE)
-    max <- predped::mll(data, params_max, transform = FALSE)
+    min <- predped::mll(data, params_min, transform = FALSE, summed = TRUE)
+    q25 <- predped::mll(data, params_25, transform = FALSE, summed = TRUE)
+    q75 <- predped::mll(data, params_75, transform = FALSE, summed = TRUE)
+    max <- predped::mll(data, params_max, transform = FALSE, summed = TRUE)
     
     # Ideally, the mll of the generating parameters should be lower than the 
     # mll of other parameters. Test this assumption here.
@@ -70,12 +70,12 @@ testthat::test_that("Computing the MLL with conversion works", {
     params_max <- predped::to_unbounded(params_max, bounds)
 
     # Compute the min-log-likelihood for each of the parameters
-    best <- predped::mll(data, params, transform = TRUE)
+    best <- predped::mll(data, params, transform = TRUE, summed = TRUE)
 
-    min <- predped::mll(data, params_min, transform = TRUE)
-    q25 <- predped::mll(data, params_25, transform = TRUE)
-    q75 <- predped::mll(data, params_75, transform = TRUE)
-    max <- predped::mll(data, params_max, transform = TRUE)
+    min <- predped::mll(data, params_min, transform = TRUE, summed = TRUE)
+    q25 <- predped::mll(data, params_25, transform = TRUE, summed = TRUE)
+    q75 <- predped::mll(data, params_75, transform = TRUE, summed = TRUE)
+    max <- predped::mll(data, params_max, transform = TRUE, summed = TRUE)
     
     # Ideally, the mll of the generating parameters should be lower than the 
     # mll of other parameters. Test this assumption here.
@@ -86,6 +86,9 @@ testthat::test_that("Computing the MLL with conversion works", {
 })
 
 testthat::test_that("Likelihood R and Rcpp converge", {
+    ############################################################################
+    # SUMMED
+
     # Read in some test data. Of these, select only the first 100, as these 
     # already contain all necessary ingredients (social simulation)
     data <- readRDS(file.path("data", "data_mll.Rds"))
@@ -97,16 +100,45 @@ testthat::test_that("Likelihood R and Rcpp converge", {
 
     # Create test and reference and compare both across all datapoints. Here, 
     # we don't transform the parameters
-    ref <- predped::mll(data, params, transform = FALSE, cpp = FALSE)
-    tst <- predped::mll(data, params, transform = FALSE, cpp = TRUE)
+    ref <- predped::mll(data, params, transform = FALSE, cpp = FALSE, summed = TRUE)
+    tst <- predped::mll(data, params, transform = FALSE, cpp = TRUE, summed = TRUE)
 
     testthat::expect_equal(ref, tst)
 
     # And now do one where you do transform the parameters
     params <- predped::to_unbounded(params, predped::params_from_csv[["params_bounds"]])
 
-    ref <- predped::mll(data, params, transform = TRUE, cpp = FALSE)
-    tst <- predped::mll(data, params, transform = TRUE, cpp = TRUE)
+    ref <- predped::mll(data, params, transform = TRUE, cpp = FALSE, summed = TRUE)
+    tst <- predped::mll(data, params, transform = TRUE, cpp = TRUE, summed = TRUE)
+
+    testthat::expect_equal(ref, tst)
+
+
+
+    ############################################################################
+    # RAW
+
+    # Read in some test data. Of these, select only the first 100, as these 
+    # already contain all necessary ingredients (social simulation)
+    data <- readRDS(file.path("data", "data_mll.Rds"))
+    data <- data[1:100, ]
+
+    # Retrieve the parameters of the SocialBaselineEuropean
+    params <- predped::params_from_csv[["params_archetypes"]]
+    params <- params[params$name == "SocialBaselineEuropean", ]
+
+    # Create test and reference and compare both across all datapoints. Here, 
+    # we don't transform the parameters
+    ref <- predped::mll(data, params, transform = FALSE, cpp = FALSE, summed = FALSE)
+    tst <- predped::mll(data, params, transform = FALSE, cpp = TRUE, summed = FALSE)
+
+    testthat::expect_equal(ref, tst)
+
+    # And now do one where you do transform the parameters
+    params <- predped::to_unbounded(params, predped::params_from_csv[["params_bounds"]])
+
+    ref <- predped::mll(data, params, transform = TRUE, cpp = FALSE, summed = FALSE)
+    tst <- predped::mll(data, params, transform = TRUE, cpp = TRUE, summed = FALSE)
 
     testthat::expect_equal(ref, tst)
 })
