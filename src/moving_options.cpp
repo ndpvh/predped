3b5354f4f1b3621a067e219caf38c162301371c6
ndpvh <niels.vanhasbroeck@gmail.com>
2025-09-17 11:57:00 +0200

    Implement Rcpp alternative of `compute_centers`

--- src/moving_options.cpp
+++ src/moving_options.cpp
@@ -6,6 +6,125 @@
 
 using namespace Rcpp;
 
+//' Compute cell centers 
+//' 
+//' Rcpp alternative to \code{\link[predped]{compute_centers}}.
+//'
+//' Compute cell centers based on a person's current position and velocity, 
+//' accounting for potential changes in speed and direction. Alternative to 
+//' \code{\link[m4ma]{c_vd}} that accounts for biomechanical limitations in the 
+//' speed one can maintain when turning at a greater angle. Defaults are based on 
+//' Seethapathi et al. (2024), Brown et al. (2020), and Glaister et al. (2007).
+//' 
+//' @param agent Object of the \code{\link[predped]{agent-class}}.
+//' @param a,b Numerics denoting the parameters of the weighting function, where 
+//' \code{a} is used for the power of the function and \code{b} for the slope of 
+//' function. \code{a} is required to be positive and \code{b} should lie between 
+//' 0 and 1, where \code{1 - b} denotes the maximal decrease in velocities in 
+//' percentage.
+//' @param velocities Numeric matrix containing the change in speed for an agent
+//' whenever they move to the respective cell of this matrix. 
+//' @param orientations Numeric matrix containing the change in direction for an
+//' agent whenever they move to the respective cell of this matrix. 
+//' @param time_step Numeric denoting the number of seconds each discrete step in
+//' time should mimic. Defaults to \code{0.5}, or half a second.
+//'
+//' @return Numeric matrix of (x, y) coordinates for each cell
+//' 
+//' @examples 
+//' # Create two agents, one fast and one slow
+//' slow_agent <- agent(center = c(-2.75, 0), 
+//'                     radius = 0.25, 
+//'                     speed = 0.5, 
+//'                     orientation = 0,
+//'                     current_goal = goal(position = c(-2.01, 0)))
+//' 
+//' fast_agent <- agent(center = c(-2.75, 0), 
+//'                     radius = 0.25, 
+//'                     speed = 2, 
+//'                     orientation = 0,
+//'                     current_goal = goal(position = c(-2.01, 0)))
+//' 
+//' # Generate the cell centers with predped
+//' slow_centers <- compute_centers(slow_agent,
+//'                                 cpp = TRUE)
+//' fast_centers <- compute_centers(fast_agent
+//'                                 cpp = TRUE)
+//' 
+//' # Generate the cell centers with m4ma
+//' slow_m4ma <- m4ma::c_vd(1:33, 
+//'                         position(slow_agent), 
+//'                         speed(slow_agent), 
+//'                         orientation(slow_agent))
+//' fast_m4ma <- m4ma::c_vd(1:33, 
+//'                         position(fast_agent), 
+//'                         speed(fast_agent), 
+//'                         orientation(fast_agent))
+//' 
+//' # Compare both through a plot. This should show that the predped variant 
+//' # accounts for an interaction between an agent's speed and change in 
+//' # direction when computing the cell centers
+//' base::plot(slow_centers, col = "black")
+//' graphics::points(slow_m4ma[, 1], slow_m4ma[, 2], col = "red")
+//' 
+//' base::plot(fast_centers, col = "black")
+//' graphics::points(fast_m4ma[, 1], fast_m4ma[, 2], col = "red")
+//' 
+//' 
+//' @seealso 
+//' \code{\link[predped]{agent-class}},
+//' \code{\link[m4ma]{c_vd}}
+//' \code{\link[predped]{compute_centers}}
+//' \code{\link[predped]{moving_options-method}} 
+//' 
+//' @rdname compute_centers_rcpp
+//'
+//' @export
+// [[Rcpp::export]]
+Rcpp::NumericMatrix compute_centers_rcpp(Rcpp::S4 agent, 
+                                         double a,
+                                         double b, 
+                                         Rcpp::NumericVector velocities,
+                                         Rcpp::NumericVector orientations,
+                                         double time_step = 0.5) {
+    
+    // Preallocate some variables, including the matrix in which the centers 
+    // will be saved
+    NumericMatrix centers(velocities.length(), 2);
+    double slow = 0.;
+    double velocity = 0.;
+    double new_orientation = 0.;
+
+    // Retrieve the current speed, orientation, and position of the agent
+    NumericVector position = agent.slot("center");
+    double speed = agent.slot("speed");
+    double orientation = agent.slot("orientation");
+
+    // Transform all angles to radians
+    NumericVector angles = orientations * M_PI / 180;
+    orientation = orientation * M_PI / 180;
+
+    // Loop over all velocities and orientations
+    for(int i = 0; i < velocities.length(); i++) {
+        // Define a slowing factor that depends on the turning angle or change in 
+        // direction
+        slow = 1 - b * pow(sin(abs(angles[i])), a);
+
+        // Adjust the velocity of the agent
+        velocity = speed * slow * velocities[i] * time_step;
+
+        // Compute the new orientation of the agent
+        new_orientation = orientation + angles[i];
+
+        // Compute the centers themselves
+        centers(i, 0) = position[0] + velocity * cos(new_orientation);
+        centers(i, 1) = position[1] + velocity * sin(new_orientation);
+    }
+
+    return(centers);
+}
+
+
 //' Check agent and object overlap
 //' 
 //' Rcpp alternative to \code{\link[predped]{overlap_with_objects}}. 
