3a28b2b21c18bac65b675686b0aafbdb03f7509d
ndpvh <niels.vanhasbroeck@gmail.com>
2025-10-31 17:11:00 +0100

    Remove dependency to `data.table`

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -1,7 +1,6 @@
 testthat::test_that("Transforming to time series works", {
     trace <- readRDS(file.path(".", "data", "example_trace.Rds"))
-    ref <- data.table::fread(file.path(".", "data", "example_time_series.csv"), 
-                             data.table = FALSE)
+    ref <- read.table(file.path(".", "data", "example_time_series.csv"))
 
     tst <- predped::time_series(trace)
 
