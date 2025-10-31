5f71067565d8f7986e58dc281be782597e8b9702
ndpvh <niels.vanhasbroeck@gmail.com>
2025-10-31 15:34:43 +0100

    Fix test: Make testthat not skip them, but remove them altogether

--- tests/testthat/test_likelihood.R
+++ tests/testthat/test_likelihood.R
@@ -1,91 +1,91 @@
-testthat::test_that("Computing the MLL works", {
-    # Get the data to be used
-    data <- readRDS(file.path("data", "data_mll.Rds"))
-    data <- data[!is.na(data$ps_speed), ]
+# testthat::test_that("Computing the MLL works", {
+#     # Get the data to be used
+#     data <- readRDS(file.path("data", "data_mll.Rds"))
+#     data <- data[!is.na(data$ps_speed), ]
 
-    # Extract the simulating parameters
-    params <- predped::params_from_csv[["params_archetypes"]][1, -(1:2)]
-    bounds <- predped::params_from_csv[["params_bounds"]]
+#     # Extract the simulating parameters
+#     params <- predped::params_from_csv[["params_archetypes"]][1, -(1:2)]
+#     bounds <- predped::params_from_csv[["params_bounds"]]
 
-    # Use the bounds to make some other parameters
-    params_min <- as.data.frame(t(bounds[,1]))
-    params_max <- as.data.frame(t(bounds[,2]))
-    params_25 <- as.data.frame(t(bounds[,1] + 0.25 * (bounds[,2] - bounds[,1])))
-    params_75 <- as.data.frame(t(bounds[,1] + 0.75 * (bounds[,2] - bounds[,1])))
+#     # Use the bounds to make some other parameters
+#     params_min <- as.data.frame(t(bounds[,1]))
+#     params_max <- as.data.frame(t(bounds[,2]))
+#     params_25 <- as.data.frame(t(bounds[,1] + 0.25 * (bounds[,2] - bounds[,1])))
+#     params_75 <- as.data.frame(t(bounds[,1] + 0.75 * (bounds[,2] - bounds[,1])))
 
-    params_min$b_current_direction <- 1
-    params_25$b_current_direction <- 1
-    params_75$b_current_direction <- 1
-    params_max$b_current_direction <- 1
+#     params_min$b_current_direction <- 1
+#     params_25$b_current_direction <- 1
+#     params_75$b_current_direction <- 1
+#     params_max$b_current_direction <- 1
 
-    params_min$a_current_direction <- 2
-    params_25$a_current_direction <- 2
-    params_75$a_current_direction <- 2
-    params_max$a_current_direction <- 2
+#     params_min$a_current_direction <- 2
+#     params_25$a_current_direction <- 2
+#     params_75$a_current_direction <- 2
+#     params_max$a_current_direction <- 2
 
-    # Compute the min-log-likelihood for each of the parameters
-    best <- predped::mll(data, params, transform = FALSE, summed = TRUE)
+#     # Compute the min-log-likelihood for each of the parameters
+#     best <- predped::mll(data, params, transform = FALSE, summed = TRUE)
 
-    min <- predped::mll(data, params_min, transform = FALSE, summed = TRUE)
-    q25 <- predped::mll(data, params_25, transform = FALSE, summed = TRUE)
-    q75 <- predped::mll(data, params_75, transform = FALSE, summed = TRUE)
-    max <- predped::mll(data, params_max, transform = FALSE, summed = TRUE)
+#     min <- predped::mll(data, params_min, transform = FALSE, summed = TRUE)
+#     q25 <- predped::mll(data, params_25, transform = FALSE, summed = TRUE)
+#     q75 <- predped::mll(data, params_75, transform = FALSE, summed = TRUE)
+#     max <- predped::mll(data, params_max, transform = FALSE, summed = TRUE)
     
-    # Ideally, the mll of the generating parameters should be lower than the 
-    # mll of other parameters. Test this assumption here.
-    # testthat::expect_true(all(best <= min))
-    # testthat::expect_true(all(best <= q25))
-    # testthat::expect_true(all(best <= q75))
-    # testthat::expect_true(all(best <= max))
-})
+#     # Ideally, the mll of the generating parameters should be lower than the 
+#     # mll of other parameters. Test this assumption here.
+#     testthat::expect_true(all(best <= min))
+#     testthat::expect_true(all(best <= q25))
+#     testthat::expect_true(all(best <= q75))
+#     testthat::expect_true(all(best <= max))
+# })
 
-testthat::test_that("Computing the MLL with conversion works", {
-    # Get the data to be used
-    data <- readRDS(file.path("data", "data_mll.Rds"))
-    data <- data[!is.na(data$ps_speed), ]
+# testthat::test_that("Computing the MLL with conversion works", {
+#     # Get the data to be used
+#     data <- readRDS(file.path("data", "data_mll.Rds"))
+#     data <- data[!is.na(data$ps_speed), ]
 
-    # Extract the simulating parameters
-    params <- predped::params_from_csv[["params_archetypes"]][1, -(1:2)]
-    bounds <- predped::params_from_csv[["params_bounds"]]
+#     # Extract the simulating parameters
+#     params <- predped::params_from_csv[["params_archetypes"]][1, -(1:2)]
+#     bounds <- predped::params_from_csv[["params_bounds"]]
 
-    # Use the bounds to make some other parameters
-    params_min <- as.data.frame(t(bounds[,1]))
-    params_max <- as.data.frame(t(bounds[,2]))
-    params_25 <- as.data.frame(t(bounds[,1] + 0.25 * (bounds[,2] - bounds[,1])))
-    params_75 <- as.data.frame(t(bounds[,1] + 0.75 * (bounds[,2] - bounds[,1])))
+#     # Use the bounds to make some other parameters
+#     params_min <- as.data.frame(t(bounds[,1]))
+#     params_max <- as.data.frame(t(bounds[,2]))
+#     params_25 <- as.data.frame(t(bounds[,1] + 0.25 * (bounds[,2] - bounds[,1])))
+#     params_75 <- as.data.frame(t(bounds[,1] + 0.75 * (bounds[,2] - bounds[,1])))
 
-    params_min$b_current_direction <- 1
-    params_25$b_current_direction <- 1
-    params_75$b_current_direction <- 1
-    params_max$b_current_direction <- 1
+#     params_min$b_current_direction <- 1
+#     params_25$b_current_direction <- 1
+#     params_75$b_current_direction <- 1
+#     params_max$b_current_direction <- 1
 
-    params_min$a_current_direction <- 2
-    params_25$a_current_direction <- 2
-    params_75$a_current_direction <- 2
-    params_max$a_current_direction <- 2
+#     params_min$a_current_direction <- 2
+#     params_25$a_current_direction <- 2
+#     params_75$a_current_direction <- 2
+#     params_max$a_current_direction <- 2
 
-    # Convert all these parameters to their real alternatives
-    params <- predped::to_unbounded(params, bounds)
-    params_min <- predped::to_unbounded(params_min, bounds)
-    params_25 <- predped::to_unbounded(params_25, bounds)
-    params_75 <- predped::to_unbounded(params_75, bounds)
-    params_max <- predped::to_unbounded(params_max, bounds)
+#     # Convert all these parameters to their real alternatives
+#     params <- predped::to_unbounded(params, bounds)
+#     params_min <- predped::to_unbounded(params_min, bounds)
+#     params_25 <- predped::to_unbounded(params_25, bounds)
+#     params_75 <- predped::to_unbounded(params_75, bounds)
+#     params_max <- predped::to_unbounded(params_max, bounds)
 
-    # Compute the min-log-likelihood for each of the parameters
-    best <- predped::mll(data, params, transform = TRUE, summed = TRUE)
+#     # Compute the min-log-likelihood for each of the parameters
+#     best <- predped::mll(data, params, transform = TRUE, summed = TRUE)
 
-    min <- predped::mll(data, params_min, transform = TRUE, summed = TRUE)
-    q25 <- predped::mll(data, params_25, transform = TRUE, summed = TRUE)
-    q75 <- predped::mll(data, params_75, transform = TRUE, summed = TRUE)
-    max <- predped::mll(data, params_max, transform = TRUE, summed = TRUE)
+#     min <- predped::mll(data, params_min, transform = TRUE, summed = TRUE)
+#     q25 <- predped::mll(data, params_25, transform = TRUE, summed = TRUE)
+#     q75 <- predped::mll(data, params_75, transform = TRUE, summed = TRUE)
+#     max <- predped::mll(data, params_max, transform = TRUE, summed = TRUE)
     
-    # Ideally, the mll of the generating parameters should be lower than the 
-    # mll of other parameters. Test this assumption here.
-    # testthat::expect_true(all(best <= min))
-    # testthat::expect_true(all(best <= q25))
-    # testthat::expect_true(all(best <= q75))
-    # testthat::expect_true(all(best <= max))
-})
+#     # Ideally, the mll of the generating parameters should be lower than the 
+#     # mll of other parameters. Test this assumption here.
+#     testthat::expect_true(all(best <= min))
+#     testthat::expect_true(all(best <= q25))
+#     testthat::expect_true(all(best <= q75))
+#     testthat::expect_true(all(best <= max))
+# })
 
 testthat::test_that("Likelihood R and Rcpp converge", {
     ############################################################################
